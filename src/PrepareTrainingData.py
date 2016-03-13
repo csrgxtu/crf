b48e2b7a5fed936c6bd3015af37540460e80231d
archer <1246506786@qq.com>
2016-03-13 14:29:23 +0800

    temp commit

--- src/PrepareTrainingData.py
+++ src/PrepareTrainingData.py
@@ -6,6 +6,9 @@
 # File: PrepareTrainingData.py
 # Desc: preparing the training data for CRF++
 #
+# Format: each row is a token, consist of the following
+# GameName HomeAway FGM FGA 3PM 3PA FTM FTA OREB DREB AST TOV STL BLK PF PTS WinLose
+#
 # Produced By CSRGXTU
 import sys
 
@@ -21,4 +24,26 @@ DATA_PATH = '../data/TeamRank/'
 
 mat = loadMatrixFromFile(DATA_PATH + teamid + '.csv.sorted')
 
-print mat[0]
+# print mat[0]
+trainMat = []
+for row in mat:
+  tmp = []
+  # GameName
+  tmp.append()
+
+  # HomeAway
+  # FGM
+  # FGA
+  # 3PM
+  # 3PA
+  # FTM
+  # FTA
+  # OREB
+  # DREB
+  # AST
+  # TOV
+  # STL
+  # BLK
+  # PF
+  # PTS
+  # WinLose
