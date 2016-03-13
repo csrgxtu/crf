59e8129a9e27f6e2942a9f70083f31f08c9005e0
archer <1246506786@qq.com>
2016-03-13 11:10:56 +0800

    PrepareTrainingData.py

--- src/PrepareTrainingData.py
+++ src/PrepareTrainingData.py
@@ -7,7 +7,19 @@
 # Desc: preparing the training data for CRF++
 #
 # Produced By CSRGXTU
-from Utility import loadSeasons
+from Utility import loadSeasons, loadTeamIds
 
-seasons = loadSeasons("../data/TeamRank/seasons.csv")
-print seasons
+DATA_PATH = '../data/TeamRank/'
+
+seasons = loadSeasons(DATA_PATH + 'seasons.csv')
+# print seasons
+
+teams = loadTeamIds(DATA_PATH + 'teamidshortname.csv')
+# print teams
+
+files = []
+for team in teams:
+  for season in seasons:
+    files.append(DATA_PATH + team + '.csv.sorted.' + season + '.csv')
+
+print files
